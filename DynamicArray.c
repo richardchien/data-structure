6db0a60599889a8e0a94c1425812426eacb3db1c
Richard Chien <richardchienthebest@gmail.com>
2015-08-12 23:02:40 +0800

    improve DynamicArray

--- DynamicArray.c
+++ DynamicArray.c
@@ -71,22 +71,94 @@ Array *ArrayCopyArray(Array *pArr) {
     return ArraySubArray(pArr, 0, pArr->length);
 }
 
+#pragma mark - Get Properties
+
+int ArrayLength(Array *pArr) {
+    return pArr->length;
+}
+
+int ArrayItemSize(Array *pArr) {
+    return pArr->itemSize;
+}
+
 #pragma mark - Manipulate Whole Array
 
 void ArrayDestroy(Array *pArr) {
-    if (pArr) {
-        if (pArr->pData) {
-            free(pArr->pData);
-            pArr->pData = NULL;
-        }
-        free(pArr);
+    if (!pArr) {
+        return;
     }
+    
+    if (pArr->pData) {
+        free(pArr->pData);
+        pArr->pData = NULL;
+    }
+    free(pArr);
 }
 
 void ArrayTraverse(Array *pArr, void (*pFunc)(const void *)) {
+    if (!pArr) {
+        return;
+    }
+    
     for (int i = 0; i < pArr->length; i++) {
         pFunc(itemAt(pArr, i));
     }
+}
+
+bool ArraySort(Array *pArr, int (*pCompareFunc)(const void *, const void *), bool ascend) {
+    if (!pArr) {
+        return false;
+    }
+    
+    // Bubble sort
+    for (int i = 0; i < pArr->length - 1; i++) {
+        for (int j = 0; j < pArr->length - 1 - i; j++) {
+            if (ascend) {
+                if (0 < pCompareFunc(itemAt(pArr, j), itemAt(pArr, j + 1))) {
+                    if (!ArraySwapItems(pArr, j, j + 1)) {
+                        // Probably mess up the original order,
+                        // if already swaped some items
+                        return false;
+                    }
+                }
+            } else {
+                if (0 > pCompareFunc(itemAt(pArr, j), itemAt(pArr, j + 1))) {
+                    if (!ArraySwapItems(pArr, j, j + 1)) {
+                        // Probably mess up the original order,
+                        // if already swaped some items
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    
+    return true;
+}
+
+bool ArrayReverse(Array *pArr) {
+    for (int i = 0; i < pArr->length / 2; i++) {
+        if (!ArraySwapItems(pArr, i, pArr->length - 1 - i)) {
+            // Probably mess up the original order,
+            // if already swaped some items
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int ArrayFind(Array *pArr, void *pVal, int (*pCompareFunc)(const void *, const void *)) {
+    if (!pArr) {
+        return -2;
+    }
+    for (int i = 0; i < pArr->length; i++) {
+        if (0 == pCompareFunc(itemAt(pArr, i), pVal)) {
+            return i;
+        }
+    }
+    
+    return -1;
 }
 
 #pragma mark - Manipulate Single Item
@@ -105,6 +177,14 @@ bool ArrayGetItem(Array *pArr, int index, void *pOut) {
     return true;
 }
 
+bool ArrayGetFirstItem(Array *pArr, void *pOut) {
+    return ArrayGetItem(pArr, 0, pOut);
+}
+
+bool ArrayGetLastItem(Array *pArr, void *pOut) {
+    return ArrayGetItem(pArr, pArr->length - 1, pOut);
+}
+
 bool ArraySetItem(Array *pArr, int index, void *pIn) {
     if (!pArr) {
         return false;
@@ -121,6 +201,10 @@ bool ArraySetItem(Array *pArr, int index, void *pIn) {
 
 bool ArrayInsertItem(Array *pArr, int index, void *pIn) {
     if (!pArr) {
+        return false;
+    }
+    
+    if (index < 0 || index > pArr->length) {
         return false;
     }
     
@@ -153,6 +237,10 @@ bool ArrayAppendItem(Array *pArr, void *pIn) {
     return true;
 }
 
+bool ArrayPrependItem(Array *pArr, void *pIn) {
+    return ArrayInsertItem(pArr, 0, pIn);
+}
+
 bool ArrayMoveItem(Array *pArr, int oldIndex, int newIndex) { // TODO
     if (!pArr) {
         return false;
@@ -162,28 +250,30 @@ bool ArrayMoveItem(Array *pArr, int oldIndex, int newIndex) { // TODO
         return false;
     }
     
-    if (oldIndex != newIndex) {
-        void *pTemp = malloc(pArr->itemSize);
-        if (!pTemp) {
-            return false;
-        }
-        
-        memcpy(pTemp, itemAt(pArr, oldIndex), pArr->itemSize);
-        
-        if (oldIndex > newIndex) {
-            for (int i = oldIndex; i > newIndex; i--) {
-                ArrayReplaceItemAWithB(pArr, i, i - 1);
-            }
-        } else {
-            for (int i = oldIndex; i < newIndex; i++) {
-                ArrayReplaceItemAWithB(pArr, i, i + 1);
-            }
-        }
-        
-        ArraySetItem(pArr, newIndex, pTemp);
-        
-        free(pTemp);
+    if (oldIndex == newIndex) {
+        return true;
     }
+    
+    void *pTemp = malloc(pArr->itemSize);
+    if (!pTemp) {
+        return false;
+    }
+    
+    memcpy(pTemp, itemAt(pArr, oldIndex), pArr->itemSize);
+    
+    if (oldIndex > newIndex) {
+        for (int i = oldIndex; i > newIndex; i--) {
+            ArrayReplaceItemAWithB(pArr, i, i - 1);
+        }
+    } else {
+        for (int i = oldIndex; i < newIndex; i++) {
+            ArrayReplaceItemAWithB(pArr, i, i + 1);
+        }
+    }
+    
+    ArraySetItem(pArr, newIndex, pTemp);
+    
+    free(pTemp);
     
     return true;
 }
@@ -197,22 +287,24 @@ bool ArraySwapItems(Array *pArr, int aIndex, int bIndex) {
         return false;
     }
     
-    if (aIndex != bIndex) {
-        void *pA = itemAt(pArr, aIndex);
-        void *pB = itemAt(pArr, bIndex);
-        
-        void *pTemp = malloc(pArr->itemSize);
-        if (!pTemp) {
-            return false;
-        }
-        
-        memcpy(pTemp, pB, pArr->itemSize);
-        
-        ArraySetItem(pArr, bIndex, pA);
-        ArraySetItem(pArr, aIndex, pTemp);
-        
-        free(pTemp);
+    if (aIndex == bIndex) {
+        return true;
     }
+    
+    void *pA = itemAt(pArr, aIndex);
+    void *pB = itemAt(pArr, bIndex);
+    
+    void *pTemp = malloc(pArr->itemSize);
+    if (!pTemp) {
+        return false;
+    }
+    
+    memcpy(pTemp, pB, pArr->itemSize);
+    
+    ArraySetItem(pArr, bIndex, pA);
+    ArraySetItem(pArr, aIndex, pTemp);
+    
+    free(pTemp);
     
     return true;
 }
@@ -226,9 +318,11 @@ bool ArrayReplaceItemAWithB(Array *pArr, int aIndex, int bIndex) {
         return false;
     }
     
-    if (aIndex != bIndex) {
-        memcpy(itemAt(pArr, aIndex), itemAt(pArr, bIndex), pArr->itemSize);
+    if (aIndex == bIndex) {
+        return true;
     }
+    
+    memcpy(itemAt(pArr, aIndex), itemAt(pArr, bIndex), pArr->itemSize);
     
     return true;
 }
--- DynamicArray.h
+++ DynamicArray.h
@@ -1,6 +1,6 @@
 //
 //  DynamicArray.h
-//  
+//
 //
 //  Created by Richard Chien on 8/11/15.
 //
@@ -20,21 +20,38 @@ typedef struct {
     int itemSize;
 } Array;
 
-// Make Array
+#pragma mark - Make Array
+
 Array *ArrayInit(int itemSize);
 Array *ArrayInitWithLength(int itemSize, int initLen);
 Array *ArraySubArray(Array *pArr, int start, int length);
 Array *ArrayCopyArray(Array *pArr);
 
-// Manipulate Whole Array
+#pragma mark - Get Properties
+
+int ArrayLength(Array *pArr);
+int ArrayItemSize(Array *pArr);
+
+#pragma mark - Manipulate Whole Array
+
 void ArrayDestroy(Array *pArr);
 void ArrayTraverse(Array *pArr, void (*pFunc)(const void *));
+// Probably mess up the original order if memory is not enough
+bool ArraySort(Array *pArr, int (*pCompareFunc)(const void *, const void *), bool ascend);
+// Probably mess up the original order if memory is not enough
+bool ArrayReverse(Array *pArr);
+// Return -1 if no such item, return -2 if pArr == NULL
+int  ArrayFind(Array *pArr, void *pVal, int (*pCompareFunc)(const void *, const void *));
 
-// Manipulate Single Item
+#pragma mark - Manipulate Single Item
+
 bool ArrayGetItem(Array *pArr, int index, void *pOut);
+bool ArrayGetFirstItem(Array *pArr, void *pOut);
+bool ArrayGetLastItem(Array *pArr, void *pOut);
 bool ArraySetItem(Array *pArr, int index, void *pIn);
 bool ArrayInsertItem(Array *pArr, int index, void *pIn);
 bool ArrayAppendItem(Array *pArr, void *pIn);
+bool ArrayPrependItem(Array *pArr, void *pIn);
 bool ArrayMoveItem(Array *pArr, int oldIndex, int newIndex);
 bool ArraySwapItems(Array *pArr, int aIndex, int bIndex);
 bool ArrayReplaceItemAWithB(Array *pArr, int aIndex, int bIndex);
