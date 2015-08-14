2011c68f9c67b4d65cd2c0213befd6a67cff6ccb
Richard Chien <richardchienthebest@gmail.com>
2015-08-14 11:41:16 +0800

    add String

---
+++ String.c
@@ -0,0 +1,9 @@
+//
+//  String.c
+//  DataStructure
+//
+//  Created by Richard Chien on 8/14/15.
+//  Copyright © 2015 Richard Chien. All rights reserved.
+//
+
+#include "String.h"
---
+++ String.h
@@ -0,0 +1,14 @@
+//
+//  String.h
+//  DataStructure
+//
+//  Created by Richard Chien on 8/14/15.
+//  Copyright © 2015 Richard Chien. All rights reserved.
+//
+
+#ifndef String_h
+#define String_h
+
+#include <stdio.h>
+
+#endif /* String_h */
