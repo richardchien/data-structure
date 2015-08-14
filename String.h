b676f31dd981f9ec7e52494ff864aa2678a26df9
Richard Chien <richardchienthebest@gmail.com>
2015-08-15 00:34:33 +0800

    definition of String done

--- String.h
+++ String.h
@@ -31,12 +31,16 @@ int StringLength(const String *pStr);
 
 #pragma mark - Manipulate Whole String
 
+void StringTraverse(String *pStr, void (*pFunc)(void *));
+
+#pragma mark ---Modify
 void StringDestroy(String *pStr);
 void StringClear(String *pStr);
-void StringTraverse(String *pStr, void (*pFunc)(void *));
-void StringPrint(const String *pStr);
 // Probably mess up the original order if memory is not enough
 bool StringReverse(String *pStr);
+
+#pragma mark ---Do Not Modify
+void StringPrint(const String *pStr);
 // Return -1 if no such character, return -2 if pStr == NULL
 int  StringFindCharacter(const String *pStr, char ch);
 // Return -1 if no such substring, return -2 if pStr == NULL
@@ -47,6 +51,7 @@ int  StringCompare(const String *pStrA, const String *pStrB);
 
 #pragma mark - Manipulate Single Character
 
+#pragma mark ---Get
 bool StringGetCharacter(const String *pStr, int index, char *pOut);
 bool StringGetFirstCharacter(const String *pStr, char *pOut);
 bool StringGetLastCharacter(const String *pStr, char *pOut);
@@ -54,30 +59,40 @@ char StringCharacter(const String *pStr, int index);
 char StringFirstCharacter(const String *pStr);
 char StringLastCharacter(const String *pStr);
 
+#pragma mark ---Replace
 bool StringReplaceCharacter(String *pStr, int index, char ch);
+bool StringReplaceCharacterAWithB(String *pStr, int aIndex, int bIndex);
 bool StringReplaceSubString(String *pStr, int index, String *pNewSub);
 bool StringReplaceSubCString(String *pStr, int index, char *pNewCSub);
+bool StringReplaceAllCharater(String *pStr, char oldCh, char newCh);
+bool StringReplaceAllSubString(String *pStr, String *pOldSub, String *pNewSub);
+bool StringReplaceAllSubCString(String *pStr, char *pOldCSub, char *pNewCSub);
 
+#pragma mark ---Insert
 // Accept index range from 0 to pStr->length
 bool StringInsertCharacter(String *pStr, int index, char ch);
 bool StringInsertString(String *pStr, int index, String *pNewStr);
 bool StringInsertCString(String *pStr, int index, char *pNewCStr);
 
+#pragma mark ---Append & Prepend
 bool StringAppendCharacter(String *pStr, char ch);
 bool StringAppendString(String *pStr, String *pNewStr);
 bool StringAppendCString(String *pStr, char *pNewCStr);
-
 bool StringPrependCharacter(String *pStr, char ch);
 bool StringPrependString(String *pStr, String *pNewStr);
 bool StringPrependCString(String *pStr, char *pNewCStr);
 
+#pragma mark ---Move & Swap
 bool StringMoveCharacter(String *pStr, int oldIndex, int newIndex);
 bool StringSwapCharacters(String *pStr, int aIndex, int bIndex);
-bool StringReplaceCharacterAWithB(String *pStr, int aIndex, int bIndex);
 
+#pragma mark ---Delete
 bool StringDeleteCharacter(String *pStr, int index);
 bool StringDeleteSubString(String *pStr, int start, int length);
 
-// Trim
+#pragma mark ---Trim
+bool StringTrimCharacter(String *pStr, char ch);
+// Trim blank characters
+bool StringTrim(String *pStr);
 
 #endif
